b4a8d3b9698a22e0fade9c91dcbfe98102c4195b
Kamal <kamal.abdullayev@bestsolutions.az>
2024-01-23 15:29:25 +0400

    PalindromeNumber

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -3,43 +3,29 @@ import java.util.List;
 
 public class PalindromeNumber {
     public static void main(String[] args) {
-        int x = 2000000002;
+        int x = 321;
         int x2 = -121;
         int x3 = 10;
         int x4 = 0;
+        int x5 = 2000000002;
+        int x6 = 2111111112;
 
         System.out.println(isPalindrome(x));
-        System.out.println(isPalindrome(x2));
-        System.out.println(isPalindrome(x3));
-        System.out.println(isPalindrome(x4));
+        System.out.println(isPalindrome(x2));.
     }
 
     public static boolean isPalindrome(int x) {
-//        System.out.println(321/100);
-//        System.out.println(21/10);
-//        System.out.println(1%10);
-
-        boolean isPalindrome = true;
         if (x < 0) return false;
-        List<Integer> nums = new ArrayList<>();
 
-        int count = 1;
-        int extra;
-        while (x > 0) {
-            int temp = x;
-            extra = temp % (10*count);
-//            if (extra > 0) x-=extra;
-//            else x/= 10;
-            x-=extra;
-            System.out.println(extra / count);
-            nums.add(extra / count);
-            count*=10;
+        long reversed = 0;
+        long temp = x;
 
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
         }
 
-        for (int i = 0; i < nums.size()/2; i++) {
-            if (!nums.get(i).equals(nums.get(nums.size() - i-1))) return false;
-        }
-        return isPalindrome;
+        return (reversed == x);
     }
 }
